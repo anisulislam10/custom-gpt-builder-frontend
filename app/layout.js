21fb2e34352fc921eee786242fd7aed018b9e1fd
community-vercel <community-vercel>
2025-07-01 11:01:41 -0700

    aa

--- app/layout.js
+++ app/layout.js
@@ -44,9 +44,10 @@ export default function RootLayout({ children }) {
           ) : (
             <DashboardLayout>
               {children}
+              
               {showIframe && (
             <iframe
-  src="http://localhost:5000/api/chatbot/6839fd37577ced143770b4a3/6839e0287f90d8c892f0c2cf?domain=http%3A%2F%2Flocalhost:3  000/"
+  src="http://165.227.120.144/api/chatbot/6839fd37577ced143770b4a3/6839e0287f90d8c892f0c2cf?domain=http%3A%2F%2Flocalhost:3  000/"
   style={{
     width: '400px',
     height: '600px',
