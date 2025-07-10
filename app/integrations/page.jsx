5b147b62aafcffc064bed5792fb9261263e3007c
community-vercel <community-vercel>
2025-07-10 11:36:27 -0700

    aaa

--- app/integrations/page.jsx
+++ app/integrations/page.jsx
@@ -4,7 +4,6 @@ import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 import SmtpModal from '../components/Smtp';
 import ApiConfigModal from '../components/APiConfig';
-
 export default function SettingsPage() {
   const router = useRouter();
   const { data: session, status } = useSession({
